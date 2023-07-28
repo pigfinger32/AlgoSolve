b2d37e102b1bdfdd4588a886cf4f085e78f86fd9
wnghks517 <wnghks517@gmail.com>
2023-07-28 10:28:18 +0900

    백준 2667 단지번호붙이 BFS 1.입력받은 map 초기화 2.BFS시작할때 0,0부터 완전탐색하고 지나간 지역은 1 -> 0로 바꿈 3.시작하면 1인 사각형 cnt++, 본인을 Q에 넣어줍니다. 4.인접한 사각형을 Q에 넣어주고 넓이 갯수를 더해주고    지나간 지역은 1 -> 0로 바꿈

--- BackJun 2667 Put Number BFS/src/Main.java
+++ BackJun 2667 Put Number BFS/src/Main.java
@@ -1,58 +1,72 @@
 import java.util.*;
+
+class Point {
+    public int x;
+    public int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
 public class Main {
-    static int L, C;
-    static char[] pm, arr, tmpSortArr;
-    static int[] ch;
+    static int n;
+    static int[][] map;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static int cnt = 0;
+    static int sum = 0;
     static ArrayList<Integer> sizeArr = new ArrayList<>();
-
-    public boolean valid(char[] pm) {
-        int mo = 0;
-        int ja = 0;
-        for (char c : pm) {
-            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
-                mo++;
-            }else {ja++;}
-        }
-        if (mo >= 1 && ja >= 2) {
-            return true;
-        }else
-            return false;
-    }
-
-    public void DFS(int level, int start) {
-        if (level == L) {
-            if (valid(pm) == true) {
-                for (char c : pm) {
-                    System.out.print(c);
+    public void BFS() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (map[i][j] == 1) {
+                    sizeArr.add(BFS(i, j));
+                    cnt++;
                 }
-                System.out.println();
-            }
-        }
-        else {
-            for (int i = start; i < C; i++) {
-                pm[level] = arr[i];
-                DFS(level + 1,i+1);
             }
         }
     }
-
+    public int BFS(int x, int y) {
+        Queue<Point> q = new LinkedList<>();
+        q.offer(new Point(x, y));
+        map[x][y] = 0;
+        int sum = 1;
+        while (!q.isEmpty()) {
+            Point p = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int nx = p.x + dx[i];
+                int ny = p.y + dy[i];
+                if ((nx < n) && (nx >= 0) && (ny < n) && (ny >= 0) && (map[nx][ny] == 1)) {
+                    q.offer(new Point(nx, ny));
+                    sum++;
+                    map[nx][ny] = 0;
+                }
+            }
+        }
+        return sum;
+    }
     public static void main(String[] args) {
         Main main = new Main();
         Scanner kb=new Scanner(System.in);
-        L = kb.nextInt();//nXn
-        C = kb.nextInt();//nXn
-        arr = new char[C];
-        pm = new char[L];
-        ch = new int[C];
-        tmpSortArr = new char[L];
-
+        n = kb.nextInt();//nXn
+        map = new int[n][n];
         //전체 사각형 입력 받기
-        for(int i=0; i<C; i++){
-            arr[i] = kb.next().charAt(0);
+        for(int i=0; i<n; i++){
+            String input = kb.next();
+            for(int j=0; j<n; j++){
+                map[i][j] = input.charAt(j)-'0';
+            }
         }
-        Arrays.sort(arr);
-        main.DFS(0,0);
 
+        main.BFS();
+        Collections.sort(sizeArr);
 
+        System.out.println(cnt);
+
+        for (Integer i : sizeArr) {
+            System.out.print(i+ " ");
+        }
     }
 }
