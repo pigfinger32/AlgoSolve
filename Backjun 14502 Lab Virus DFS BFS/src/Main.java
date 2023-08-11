4ad0b8111967b7a83b5863d917458048441ff66b
wnghks517 <wnghks517@gmail.com>
2023-08-11 09:29:52 +0900

    백준 14502 연구소 바이러스 1.벽이 세워질수 있는 수열을 모두 구한다.   벽을 0,0 0,1 0,2에 세워보자.그다음은 0,0 0,1 0,3 중복되지 않게 벽을세우는거야.   근데 그벽이 원래 벽자리거나 바이러스가 아니어야 하니 0(빈공간)이면 벽을 세우는거지   그럼 중복되지 않으면서 빈공간에 벽을 세운 수열의 수가 완성되겠지. 2.바이러스가 움직이면서 움직인 공간(배열)을 모두 2로변경한다.   1.배열복사   2.tmpArr에 벽을 그리기   3.foreach 바이러스를 하나씩 꺼내서 바이러스 감염시켜본다. 3.배열을 모두 돌면서 0을 카운트한다.   1.최대값을 갱신한다.

--- Backjun 14502 Lab Virus DFS BFS/src/Main.java
+++ Backjun 14502 Lab Virus DFS BFS/src/Main.java
@@ -1,13 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static int N, M;
+    static int count = Integer.MIN_VALUE;
     static int[][] ch;
     static Point[] pm;
-    static int[][] arr;
+    static int[][] arr, tmpArr;
     static List<Point[]> wallList;
+    static List<Point> virusList;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
 
     public void DFS(int L) {
         if (L == 3) {
@@ -44,21 +46,59 @@ public class Main {
         ch = new int[N][M];
         pm = new Point[3];
         arr = new int[N][M];
+        tmpArr = new int[N][M];
         wallList = new ArrayList<>();
+        virusList = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                arr[i][j] = kb.nextInt();
+                int value = kb.nextInt();
+                arr[i][j] = value;
+                if (value == 2) {
+                    virusList.add(new Point(i, j));
+                }
             }
         }
 
-        main.DFS(0);
+        main.DFS(0);//수열만들기
         for (Point[] points : wallList) {
-            for (Point point : points) {
-                System.out.print("(" + point.x +"," + point.y +") ");
+            //1.배열복사
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    tmpArr[i][j] = arr[i][j];
+                }
             }
-            System.out.println();
+            //2.arr에 벽을 그려넣기
+            for (Point point : points) {
+                tmpArr[point.x][point.y] =1;
+            }
+            //3.foreach 바이러스를 하나씩 꺼내서 바이러스 감염시켜본다.
+            for (Point virus : virusList) {
+                Queue<Point> Q = new LinkedList<>();
+                Q.add(virus);
+                while (!Q.isEmpty()) {
+                    Point vir = Q.poll();
+                    for (int i = 0; i < 4; i++) {
+                        int nx = vir.x + dx[i];
+                        int ny = vir.y + dy[i];
+                        if (nx >= 0 && nx < N && ny >= 0 && ny < M && tmpArr[nx][ny] == 0 ) {
+                            Q.add(new Point(nx, ny));
+                            tmpArr[nx][ny] = 2;
+                        }
+                    }
+                }
+            }
+            int zeroCount = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (tmpArr[i][j] == 0) {
+                        zeroCount++;
+                    }
+                }
+            }
+            count = Integer.max(count, zeroCount);
         }
+        System.out.println(count);
     }
 }
 
